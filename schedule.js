82c25acb736ab5618b2a485b2d3b06dc033ff188
xDevJ <thexwizz@gmail.com>
2026-06-23 20:29:04 -0400

    schedule.js

---
+++ schedule.js
@@ -0,0 +1,13 @@
+const pickUpOnly = document.getElementById('pickup-only');
+const pickUpDelivery = document.querySelector('#pickup-delivery');
+const form = document.querySelector('#form');
+pickUpOnly.addEventListener('click', renderForm);
+pickUpDelivery.addEventListener('click', renderForm);
+
+
+function renderForm(e){
+
+    console.log(`${e.target.textContent} has been clicked`)
+    
+}
+
